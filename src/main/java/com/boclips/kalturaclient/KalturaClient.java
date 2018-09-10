2c52b2af575f8f261c0e7832e31fcc9c0ff9b153
Benjamin Gröhbiel <ben@boclips.com>
2018-09-10 14:15:03 +0100

    wip
    Signed-off-by: Jacek Rzeniewicz <jacek@boclips.com>

--- src/main/java/com/boclips/kalturaclient/KalturaClient.java
+++ src/main/java/com/boclips/kalturaclient/KalturaClient.java
@@ -1,13 +1,43 @@
 package com.boclips.kalturaclient;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.io.IOException;
+import java.util.List;
 
 public class KalturaClient {
     private KalturaClientConfig config;
 
     public KalturaClient(KalturaClientConfig config) {
         this.config = config;
+        Unirest.setObjectMapper(new ObjectMapper() {
+            private com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper
+                    = new com.fasterxml.jackson.databind.ObjectMapper();
+
+            {
+                jacksonObjectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            }
+
+            public <T> T readValue(String value, Class<T> valueType) {
+                try {
+                    return jacksonObjectMapper.readValue(value, valueType);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            public String writeValue(Object value) {
+                try {
+                    return jacksonObjectMapper.writeValueAsString(value);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
     public KalturaSession generateSession(int ttlSeconds) {
@@ -26,6 +56,23 @@ public class KalturaClient {
         } catch (UnirestException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public List<MediaEntry> mediaEntriesByReferenceIds(String... referenceIds) {
+        try {
+            MediaList mediaList = Unirest.get(this.config.getBaseUrl() + "/api_v3/service/media/action/list")
+                    .queryString("ks", "1")
+                    .queryString("filter[referenceIdIn]", String.join(",", referenceIds))
+                    .queryString("format", "1")
+                    .asObject(MediaList.class)
+                    .getBody();
+
+            return mediaList.objects;
+
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     private String tokenWithoutQuotes(String token) {
--- src/test/groovy/com/boclips/kalturaclient/KalturaClientContractTest.groovy
+++ src/test/groovy/com/boclips/kalturaclient/KalturaClientContractTest.groovy
@@ -6,7 +6,7 @@ import spock.lang.Specification
 
 class KalturaClientContractTest extends Specification {
 
-    def "returns a session"() {
+    def "returns a kaltura session"() {
         given:
         KalturaClientConfig config = KalturaClientConfig.builder()
                 .baseUrl("http://localhost:9999")
@@ -17,7 +17,7 @@ class KalturaClientContractTest extends Specification {
         KalturaClient kalturaClient = new KalturaClient(config)
 
         when:
-        PactVerificationResult result = mockTransactionSteps().runTest() {
+        PactVerificationResult result = mockSessionGeneration().runTest() {
             KalturaSession session = kalturaClient.generateSession(86400)
 
             assert session.token == "aSession"
@@ -27,7 +27,29 @@ class KalturaClientContractTest extends Specification {
         assert result == PactVerificationResult.Ok.INSTANCE
     }
 
-    static mockTransactionSteps() {
+    def "returns a list of media entries filtered by reference id"() {
+        given:
+        KalturaClientConfig config = KalturaClientConfig.builder()
+                .baseUrl("http://localhost:9999")
+                .partnerId("abc")
+                .secret("123")
+                .userId("user@kaltura.com")
+                .build()
+        KalturaClient kalturaClient = new KalturaClient(config)
+
+        when:
+        PactVerificationResult result = mockMediaList().runTest() {
+            List<MediaEntry> mediaEntries = kalturaClient.mediaEntriesByReferenceIds("2526940")
+
+            MediaEntry mediaEntry = mediaEntries.get(0)
+            assert mediaEntry.id == "_1234assd"
+        }
+
+        then:
+        assert result == PactVerificationResult.Ok.INSTANCE
+    }
+
+    static mockSessionGeneration() {
         def webapp_service = new PactBuilder()
         webapp_service {
             serviceConsumer "KalturaClient"
@@ -45,6 +67,34 @@ class KalturaClientContractTest extends Specification {
                     headers: ['Content-Type': 'application/json;charset=UTF-8'],
                     body   : '"aSession"'
             ])
+        } as PactBuilder
+    }
+
+    static mockMediaList() {
+        def webapp_service = new PactBuilder()
+        webapp_service {
+            serviceConsumer "KalturaClient"
+            hasPactWith "KalturaApi"
+            port 9999
+            uponReceiving("GET media list by reference ids")
+            withAttributes([
+                    method: 'GET',
+                    path  : '/api_v3/service/media/action/list',
+                    query : [
+                            'filter[referenceIdIn]': '2526940',
+                            'ks'                   : '123',
+                            'format'               : '1'
+                    ]
+            ])
+            willRespondWith([
+                    status : 200,
+                    headers: ['Content-Type': 'application/json'],
+            ])
+            withBody {
+                objects eachLike(1, {
+                    id identifier('_1234assd')
+                })
+            }
         } as PactBuilder
     }
 }
